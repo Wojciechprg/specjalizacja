b129ef73dc98ecfdf7818fe8d577c89a8317b7d2
xNikodem <72965661+xNikodem@users.noreply.github.com>
2020-12-18 11:45:01 +0100

    Add files via upload

--- DobryAdmin.edit.php
+++ DobryAdmin.edit.php
@@ -10,10 +10,101 @@ if (isset($_GET['zajecia'])) {
 ?>
 
 <html>
+<head>
+<style>
+	body{
+	background-image: linear-gradient(white, #99CCFF);
+	font-size:30px;
+	}
+.allbox{
+		padding:30px;
+	}
+.zajeciabox{
+}
+input[type=submit]{
+	width: 160px;
+	height: 50px;
+	background-color: #95cce6;
+	margin-left:0px;
+	margin-top:20px;
+	border-radius:20px;
+	color:black;
+	border-style: hidden;
+	padding:0;
+	outline: 0 !important;
+}
+input:hover{
+	background: rgb(106,161,187);
+	background: radial-gradient(circle, rgba(106,161,187,1) 61%, rgba(255,255,255,0.3) 93%);
+}
+.button{
+	background-color:red;
+}
+.custom-select {
+  position: relative;
+  font-family: Comic Sans;
+}
+
+.custom-select select {
+  display: none; 
+}
+
+.select-selected {
+  background-color:#95cce6;
+}
+
+.select-selected:after {
+  position: absolute;
+  content: "";
+  top: 14px;
+  right: 10px;
+  width: 0;
+  height: 0;
+  border: 6px solid transparent;
+  border-color: transparent transparent transparent transparent;
+}
+
+.select-selected.select-arrow-active:after {
+  border-color: transparent transparent transparent transparent;
+  top: 7px;
+}
+
+.select-items div,.select-selected {
+  color: #ffffff;
+  padding: 8px 16px;
+  border: 1px solid transparent;
+  border-color: transparent transparent rgba(0, 0, 100, 0.3) transparent;
+  cursor: pointer;
+  user-select: none;
+}
+
+.select-items {
+  position: absolute;
+  background-color: #95cce6;
+  top: 100%;
+  left: 0;
+  right: 0;
+  z-index: 99;
+}
+
+.select-hide {
+  display: none;
+}
+
+.select-items div:hover, .same-as-selected {
+  background-color: rgba(0, 0, 100, 0.3);
+}
+</style>
+</head>
 <body>
 
 <form action="DobryAdmin.edit.php">
-"Zajęcia: <select name=zajecia>";
+<div class="allbox">
+<div class="zajeciabox">
+Zajęcia:
+</div>
+<div class="custom-select" style="width:400px;">
+<select name=zajecia>";
 <option value=poniedzialek1>Poniedzialek1</option>
 <option value=poniedzialek2>Poniedzialek2</option>
 <option value=poniedzialek3>Poniedzialek3</option>
@@ -40,14 +131,89 @@ if (isset($_GET['zajecia'])) {
 <option value=piatek4>Piatek4</option>
 <option value=piatek5>Piatek5</option>
 </select><br>
-Typ obecności <select name=obecnosc>
+</div>
+<div class="typbox">
+Typ obecności:
+</div>
+<div class="custom-select" style="width:400px;">
+<select name=obecnosc>
 <option value=1>Obecny</option>
 <option value=2>Spozniony</option>
 <option value=3>Nieobecny</option>
 </select><br>
+</div>
 <input type="submit" value="Zmien">
-<div><a href="DobryAdmin.php">Powrót do panelu admina</a></div>
+<div><form metod="post" action="DobryAdmin.php">
+				<input type="submit" value="Powrót do panelu admina"/>
+			</form></div>
+</div>
 </form>
-
+<script>
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
+</script>
 </body>
 </html>
